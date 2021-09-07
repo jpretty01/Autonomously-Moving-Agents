e9e5dc34d7c33a7b0e446a1faa14f1b7f2ec810f
jpretty01 <jeremypretty10@gmail.com>
2021-09-07 09:38:22 -0500

    Update
    Update

--- Autonomously Moving Agents/Assets/Bot.cs
+++ Autonomously Moving Agents/Assets/Bot.cs
@@ -11,6 +11,9 @@ public class Bot : MonoBehaviour
     // Game object of the target
     public GameObject target;
 
+    // Used for Wander function
+    Vector3 wanderTarget = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,12 +34,74 @@ public class Bot : MonoBehaviour
         agent.SetDestination(this.transform.position - fleeVector);
     }
 
+    //Pursue
+    void Pursue()
+    {
+        // Determing where the target is going
+        Vector3 targetDir = target.transform.position - this.transform.position;
+
+        // Angle between forward directions
+        float relativeHeading = Vector3.Angle(this.transform.forward, this.transform.TransformVector(target.transform.forward));
+        float toTarget = Vector3.Angle(this.transform.forward, this.transform.TransformVector(targetDir));
+
+        // If the character stops moving, Seek and stop looking ahead
+        if((toTarget > 90 && relativeHeading < 20 || target.GetComponent<Drive>().currentSpeed < 0.01f))
+        {
+            Seek(target.transform.position);
+            return;
+        }
+
+        //How far to look ahead
+        float lookAhead = targetDir.magnitude / (agent.speed + target.GetComponent<Drive>().currentSpeed);
+        Seek(target.transform.position + target.transform.forward * lookAhead);
+    }
+
+    //Evade
+    void Evade()
+    {
+        // Determing where the target is going
+        Vector3 targetDir = target.transform.position - this.transform.position;
+
+        //How far to look ahead to run away
+        float lookAhead = targetDir.magnitude / (agent.speed + target.GetComponent<Drive>().currentSpeed);
+        Flee(target.transform.position + target.transform.forward * lookAhead);
+
+    }
+
+ 
+    //Wander
+    void Wander()
+    {
+        //Used for helping wander around
+        float wanderRadius = 50;
+        float wanderDistance = 20;
+        float wanderJitter = 10;
+
+        // Moves  wander target
+        wanderTarget += new Vector3(Random.Range(-1.0f, 1.0f) * wanderJitter, 0, Random.Range(-1.0f, 1.0f) * wanderJitter);
+
+        wanderTarget.Normalize();
+        wanderTarget *= wanderRadius;
+
+        // Moves local target out
+        Vector3 targetLocal = wanderTarget + new Vector3(0, 0, wanderDistance);
+        // World distance that can be seen
+        Vector3 targetWorld = this.gameObject.transform.InverseTransformVector(targetLocal);
+
+        Seek(targetWorld);
+
+    }
     // Update is called once per frame
     void Update()
     {
         // Who are we going after?
         //Seek(target.transform.position);
 
-        Flee(target.transform.position);
+        //Flee(target.transform.position);
+
+        //Pursue();
+        //Evade();
+
+        Wander();
     }
 }
